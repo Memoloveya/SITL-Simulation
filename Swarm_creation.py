1eedb443abae2494e350e5b02e65652003d45b3d
Residualstress <92587824+Residualstress@users.noreply.github.com>
2024-07-16 17:55:26 +0800

    Update Swarm_creation.py

--- Swarm_creation.py
+++ Swarm_creation.py
@@ -14,8 +14,8 @@ commands = [
     "dronekit-sitl copter --home=5.147099,100.493824,0,180",
     "dronekit-sitl copter -I1 --home=5.147036,100.493809,0,180",
 
-    "mavproxy.py --master tcp:127.0.0.1:5760 --out udp:127.0.0.1:14551 --out udp:172.17.192.1:14551",
-    "mavproxy.py --master tcp:127.0.0.1:5770 --out udp:127.0.0.1:14552 --out udp:172.17.192.1:14552",
+    "mavproxy.py --master tcp:127.0.0.1:5760 --out udp:127.0.0.1:14540 --out udp:172.17.192.1:14551",
+    "mavproxy.py --master tcp:127.0.0.1:5770 --out udp:127.0.0.1:14541 --out udp:172.17.192.1:14552",
 
 ]
 
